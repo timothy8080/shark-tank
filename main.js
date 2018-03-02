f86bac3dc782390ba1948ca8548dfa7ba3d9f2c7
Timothy <timothy.cauwe@s.stemk12.org>
2018-03-01 18:30:36 -0700

    restructure

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
