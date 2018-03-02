d46a3aba2f15f6506ec7e529b55d9f0394f21dc5
Timothy <timothy.cauwe@s.stemk12.org>
2018-03-01 18:15:53 -0700

    test

--- main.js
+++ main.js
@@ -5,6 +5,22 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 var port = normalizePort(process.env.PORT || '3000');
  
 app.listen(port);
