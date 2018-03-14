139b8b0b255d8fadccdc00bc589d317d9ee25453
timothy8080 <timothy.cauwe@gmail.com>
2018-03-13 19:06:17 -0600

    new rev

--- routes/buy-now.js
+++ routes/buy-now.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/buy', function(req, res, next) {
+  console.log("HHHHHHHHHHHHHH");
+  console.log(req.body);
   if (req.body.username){
       res.render('buy', { title: 'Express' });
   } else {
