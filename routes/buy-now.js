318547f604ad47e2b75288d35f397baf5d6ada9a
timothy8080 <timothy.cauwe@gmail.com>
2018-03-13 20:44:41 -0600

    new rev

--- routes/buy-now.js
+++ routes/buy-now.js
@@ -6,7 +6,7 @@ router.get('/buy', function(req, res, next) {
   console.log("HHHHHHHHHHHHHH");
   console.log(req.session);
   if (req.session.user && req.session.user.username){
-      res.render('buy', { title: 'Express' });
+      res.render('buy', { title: 'Express', logged:true, username:req.session.user.username });
   } else {
       res.send("Please <a href='login'>Login</a> to buy the product.")
   }
