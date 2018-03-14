f4e8a580d726788c8b59d0cb32f90228888ed0be
timothy8080 <timothy.cauwe@gmail.com>
2018-03-13 20:32:04 -0600

    new rev

--- routes/signup.js
+++ routes/signup.js
@@ -5,12 +5,16 @@ var mongolib = require("../models/mongo");
 
 /* GET home page. */
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Express', logged:false });
+  res.render('signup', { title: 'Express', logged:false, err:false });
 });
 
 router.post("/signup", function(req, res, next) {
-    mongolib.createUser(req.body.username, req.body.passwd, req.body.email, req.body.credcard);
-    res.redirect("/login");
+    if (req.body.username && req.body.passwd && req.body.email && req.body.credcard){
+      mongolib.createUser(req.body.username, req.body.passwd, req.body.email, req.body.credcard);
+      res.redirect("/login");
+    } else {
+      res.render('signup', {logged:false, err:true});
+    }
 });
 
 module.exports = router;
